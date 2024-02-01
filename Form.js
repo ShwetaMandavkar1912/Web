df0c65f8cbb3ddb2aec80a458d1c78b661044dbd
Smandavkar <158379878+ShwetaMandavkar1912@users.noreply.github.com>
2024-02-01 06:45:43 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,44 @@
+format.component.html;
+
+//  <p>template-form works!</p>
+
+// <form #myform="ngForm" (ngSubmit)="onSubmit()">
+//   Name:
+//   <input type="text" id="name" name="name" [(ngModel)]="user.name" />
+//   <br />
+
+//   Email:
+//   <input type="email" name="email" id="email" [(ngModel)]="user.email" />
+//   <br />
+
+//   Password:
+//   <input
+//     type="password"
+//     name="password"
+//     id="password"
+//     [(ngModel)]="user.password"
+//   />
+//   <br />
+
+//   <button type="submit">Login</button>
+// </form>
+
+format.component.ts;
+// export class TemplateFormComponent {
+//     user = {
+//       name: '',
+//       email: '',
+//       password: '',
+//     };
+
+//     onSubmit() {
+//       alert('Hello' + this.user.name + '!');
+//       this.resetForm();
+//     }
+//     resetForm() {
+//       this.user = {
+//         name: '',
+//         email: '',
+//         password: '',
+//       };
+//     }
